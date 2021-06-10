a7593ff04c7d29d5fbfd9d7f80546c0d3e655a3f
Brenton Hershner <brenthershner@gmail.com>
2021-06-09 22:46:58 -0700

    cleaned up addTwoNumbers

--- AddTwoNumbers/addTwoNumbers.js
+++ AddTwoNumbers/addTwoNumbers.js
@@ -17,26 +17,15 @@ const buildList = (arr) => {
 }
 
 const addTwoNumbers = (l1, l2) => {
-  // console.log(l1.val);
-  // console.log(l2.val);
-  let current1 = l1;
-  let current2 = l2;
   let p1 = l1;
   let p2 = l2;
   let sum = new ListNode();
   let sumHead = sum;
-
   let carryover = 0;
 
   while (carryover !== 0 || p1 || p2) {
     const total = (sum?.val || 0) + (p1?.val || 0) + (p2?.val || 0) + carryover;
-    // console.log('carryover', carryover);
-    // console.log('p1.val', p1.val);
-    // console.log('p2.val', p2.val);
-
     const digitSum = total % 10;
-    // console.log('digitSum', digitSum);
-
     carryover = (total - digitSum) / 10;
     sum.val = digitSum;
 
@@ -49,11 +38,8 @@ const addTwoNumbers = (l1, l2) => {
     p1 = p1?.next ? p1.next : null;
     p2 = p2?.next ? p2.next : null;
     carryover = 0;
-
   }
   return sumHead;
 };
-
-// console.log(addTwoNumbers(buildList([1]), buildList([1, 2])).val);
 
 export default addTwoNumbers;
