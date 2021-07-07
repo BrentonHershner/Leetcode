67e49eaed6b45ca39a45b621e149e39f292ddcad
Brenton Hershner <brenthershner@gmail.com>
2021-07-07 14:57:42 -0700

    cleanup generateParenthesis

--- generateParenthesis/generateParenthesis.js
+++ generateParenthesis/generateParenthesis.js
@@ -1,12 +1,9 @@
 const generateParenthesis = (n) => {
   const allPossibilities = [];
   const buildParens = (parenString, open, close) => {
-    if (parenString.length === n * 2) { 
-      allPossibilities.push(parenString);
-      return;
-    }
-    if (open < n) { buildParens(parenString + '(', open + 1, close)}
-    if (close < open) { buildParens(parenString + ')', open, close + 1)}
+    if (parenString.length === n * 2) { allPossibilities.push(parenString); }
+    if (open < n) { buildParens(parenString + '(', open + 1, close) }
+    if (close < open) { buildParens(parenString + ')', open, close + 1) }
   };
   buildParens('', 0, 0);
   return allPossibilities;
